4e19df6a5f3150ce31e5ab6e810425169115a9ee
asabrightzzz <hannamariamonika123@gmail.com>
2025-12-30 23:41:55 +0700

    vercel update analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import { BrowserRouter } from "react-router-dom";
-
-import { About, Experience, Documentations, Certificates, Hero, Navbar, Tech, Works, Footer } from "./components";
+import { Analytics } from "@vercel/analytics/next";
+import {
+  About,
+  Experience,
+  Documentations,
+  Certificates,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  Footer,
+} from "./components";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <div className='relative z-0 bg-primary'>
-        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar />
           <Hero />
         </div>
@@ -16,12 +26,13 @@ const App = () => {
         <Tech />
         <Documentations />
         <Certificates />
-        <div className='relative z-0'>
+        <div className="relative z-0">
           <Footer />
+          <Analytics />
         </div>
       </div>
     </BrowserRouter>
   );
-}
+};
 
 export default App;
